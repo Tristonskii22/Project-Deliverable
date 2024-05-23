fe31affb8767a5332c7be2b6bfe81db80e05f07f
TRISTON GLOVER <TRISTON.GLOVER9999@OMAIL.OTECH.EDU>
2024-05-23 14:41:08 -0600

    changes

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class ShoppingCart
+    {
+        private Customer _customer;
+        private ShoppingCartItem _product1;
+        private ShoppingCartItem _product2;
+        private ShoppingCartItem _product3;
+        public ShoppingCart (Customer cust)
+        {
+            _customer = cust;
+        }
+        public int GetCustomerId()
+        {
+            return _customer.GetId();
+        }
+        public ShoppingCartItem GetProductById(int id)
+        {
+            if(_product1.GetProduct().GetId() == id)
+            {
+                return _product1;
+            }
+            return null;
+        }
+        public ShoppingCartItem AddProduct(Product prod, int quantity)
+        {
+            if(quantity < 1)
+            {
+                return null;
+            }
+            if (_product1 != null && _product1.GetProduct().GetId() == prod.GetId())
+            {
+                _product1.SetQuantity(_product1.GetQuantity() + quantity);
+                return _product1;
+            }
+            if (_product1 == null)
+            {
+                _product1 = new ShoppingCartItem(prod, quantity);
+                return _product1;
+            }
+            else if 
+            return null;
+            
+        }
+        public ShoppingCartItem AddProduct(Product prod)
+        {
+            return AddProduct(prod, 1);
+        }
+
+    }
+}
