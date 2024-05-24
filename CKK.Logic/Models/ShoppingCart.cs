52d2604ff8dcc0eb8996227d6f96942d2a70fb77
TRISTON GLOVER <TRISTON.GLOVER9999@OMAIL.OTECH.EDU>
2024-05-24 11:09:15 -0600

    changes

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -26,6 +26,14 @@ namespace CKK.Logic.Models
             {
                 return _product1;
             }
+            if (_product2.GetProduct().GetId() == id)
+            {
+                return _product2;
+            }
+            if (_product3.GetProduct().GetId() == id)
+            {
+                return _product3;
+            }
             return null;
         }
         public ShoppingCartItem AddProduct(Product prod, int quantity)
@@ -44,7 +52,25 @@ namespace CKK.Logic.Models
                 _product1 = new ShoppingCartItem(prod, quantity);
                 return _product1;
             }
-            else if 
+            else if (_product2 != null && _product2.GetProduct().GetId() == prod.GetId())
+            {
+                _product2.SetQuantity(_product2.GetQuantity() + quantity);
+                return _product2;
+            }
+            if (_product1 == null)
+            {
+                _product2 = new ShoppingCartItem(prod, quantity);
+                return _product2;
+            }
+            else if (_product3 != null && _product3.GetProduct().GetId() == prod.GetId())
+            {
+                _product3.SetQuantity(_product3.GetQuantity() + quantity);
+            }
+            if (_product3 == null)
+            {
+                _product3 = new ShoppingCartItem(prod, quantity);
+                return _product3;
+            }
             return null;
             
         }
@@ -52,6 +78,73 @@ namespace CKK.Logic.Models
         {
             return AddProduct(prod, 1);
         }
-
+        public ShoppingCartItem RemoveProduct(Product prod, int quantity)
+        {
+            if (quantity < 1)
+            {
+                return null;
+            }
+            if (_product1 !=null && _product1.GetProduct().GetId() == prod.GetId())
+            {
+                _product1.SetQuantity(_product1.GetQuantity() - quantity);
+                if (_product1.GetQuantity() < 1)
+                {
+                    return null;
+                }
+                return _product1;
+            }
+            else if (_product2 !=null && _product2.GetProduct().GetId() == prod.GetId())
+            {
+                _product2.SetQuantity(_product2.GetQuantity() - quantity);
+                if (_product2.GetQuantity()<1)
+                {
+                    return null;
+                }
+                return _product2;
+            }
+            else if (_product3 !=null && _product3.GetProduct().GetId() == prod.GetId())
+            {
+                _product3.SetQuantity(_product3.GetQuantity() - quantity);
+                if (_product3.GetQuantity() < 1)
+                {
+                    return null;
+                }
+                return _product3;
+            }
+            return null;
+        }
+        public decimal GetTotal()
+        {
+            var grandtotal = 0m;
+            if (_product1 != null)
+            {
+                grandtotal += _product1.GetTotal();
+            }
+            if (_product2 != null)
+            {
+                grandtotal += _product2.GetTotal();
+            }
+            if (_product3 != null)
+            {
+                grandtotal += _product3.GetTotal();
+            }
+            return grandtotal;
+        }
+        public ShoppingCartItem GetProduct(int productNumber)
+        {
+            if (productNumber ==1)
+            {
+                return _product1;
+            }
+            if (productNumber == 2)
+            {
+                return _product2;
+            }
+            if (productNumber ==3)
+            {
+                return _product3;
+            }
+            return null;
+        }
     }
 }
